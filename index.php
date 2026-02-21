df2b693aed25d6cfaebc50222427b9cb1949b918
MarcoEstSerrano <marcoesteban116@hotmail.com>
2026-02-21 13:15:12 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+echo "buenas tardes";
 /* 
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
